78d471d22e473a4e17975d4a7769dced51656873
Alexander Poole <alex.o.poole@gmail.com>
2019-12-08 13:00:00 +0100

    Create a command line application

---
+++ GaugeRnR/__main__.py
@@ -0,0 +1,8 @@
+from .application import Application
+
+def main():
+    app = Application()
+    app.run()
+
+if __name__ == '__main__':
+    main()
---
+++ GaugeRnR/application.py
@@ -0,0 +1,25 @@
+"""Usage: arguments_example.py [-vqrh] [FILE] ...
+          arguments_example.py (--left | --right) CORRECTION FILE
+Process FILE and optionally apply correction to either left-hand side or
+right-hand side.
+Arguments:
+  FILE        optional input file
+  CORRECTION  correction angle, needs FILE, --left or --right to be present
+Options:
+  -h --help
+  -v       verbose mode
+  -q       quiet mode
+  -r       make report
+  --left   use left-hand side
+  --right  use right-hand side
+"""
+from docopt import docopt
+
+class Application():
+
+    def __init__(self):
+        arguments = docopt(__doc__)
+        print(arguments)
+    
+    def run(self):
+        print("running")
