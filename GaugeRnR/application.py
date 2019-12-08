a6079f0314bfff43268ba93fa395d92be9bf50b8
Alexander Poole <alex.o.poole@gmail.com>
2019-12-08 15:14:57 +0100

    Create a CLI API

--- GaugeRnR/application.py
+++ GaugeRnR/application.py
@@ -1,17 +1,22 @@
-"""Usage: arguments_example.py [-vqrh] [FILE] ...
-          arguments_example.py (--left | --right) CORRECTION FILE
-Process FILE and optionally apply correction to either left-hand side or
-right-hand side.
-Arguments:
-  FILE        optional input file
-  CORRECTION  correction angle, needs FILE, --left or --right to be present
+"""GaugeRnR.
+
+Usage:
+  GaugeRnR -f FILE -s STRUCTURE [-a <AXES>] [-d <DELIMITER>]
+  GaugeRnR -h | --help
+  GaugeRnR -v | --version
+
+Examples:
+  GaugeRnR -f data.csv -s5,7,11
+  GaugeRnR -f data.csv -s5,7,11 --a 1,0,2 --d ,
+
 Options:
-  -h --help
-  -v       verbose mode
-  -q       quiet mode
-  -r       make report
-  --left   use left-hand side
-  --right  use right-hand side
+  -f --file=FILE Load input data.
+  -s --structure=STRUCTURE Data structure.
+    Order should be operators, parts, measurements.
+  -a --axes=<AXES>  Order of data axes [default: 0,1,2].
+  -d --delimiter=<DELIMITER>  Order of data axes [default: ;].
+  -h --help     Show this screen.
+  -v --version  Show version.
 """
 from docopt import docopt
 
