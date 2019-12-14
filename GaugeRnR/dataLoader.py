107f5ef8a516413efa278fde9eac3509513e4645
Alexander Poole <alex.o.poole@gmail.com>
2019-12-14 09:36:57 +0100

    Fix how data loade works

--- GaugeRnR/dataLoader.py
+++ GaugeRnR/dataLoader.py
@@ -10,6 +10,10 @@ class DataLoader():
              axes,
              delimiter):
         data = np.loadtxt(fname=file, delimiter=delimiter)
-        data = data.reshape(tuple(structure))
+        s = (structure[axes[0]],
+            structure[axes[1]],
+            structure[axes[2]])
+        data = data.reshape(s)
         data = np.transpose(data, axes=axes)
+        data = data.reshape(tuple(structure))
         return data
