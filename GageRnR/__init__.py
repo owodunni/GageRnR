d4ccb8b5af8cb59d5aeb9a56c1748595b2f87793
Alexander Poole <alex.o.poole@gmail.com>
2020-01-12 16:17:46 +0100

    Update version number

--- GageRnR/__init__.py
+++ GageRnR/__init__.py
@@ -23,7 +23,7 @@ __all__ = ['GageRnR',
            'Normality',
            'Linearity', ]
 
-__version__ = "0.6.2"
+__version__ = "0.6.3"
 __version_info__ = tuple(
     int(i) for i in __version__.split(".") if i.isdigit()
 )
